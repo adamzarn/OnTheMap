38eaaaba2d6fb1ebe1699bc0f06565a40d1dd0f7
Adam Zarn <adamzarn@Adam-Zarns-MBP.att.net>
2016-08-03 14:08:25 -0500

    Post Migration Changes

--- OnTheMap/NetworkClient.swift
+++ OnTheMap/NetworkClient.swift
@@ -71,7 +71,7 @@ class NetworkClient: NSObject {
     }
     
     func getLocationData(completion: (result: [StudentInformation]?) -> ()) {
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation?limit=100")!)
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://parse.udacity.com/parse/classes/StudentLocation?limit=100")!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         let session = NSURLSession.sharedSession()
@@ -87,13 +87,12 @@ class NetworkClient: NSObject {
                         print("error")
                         return
                     }
-                    print(parsedResult)
                     if let locationsDictionary = parsedResult as? [String:AnyObject], locationsArray = locationsDictionary["results"] as? [[String:AnyObject]] {
                         performUIUpdatesOnMain {
+                            StudentInformation.studentInformationArray = []
                             for student in locationsArray {
                                 StudentInformation.studentInformationArray.append(StudentInformation(sourceData: student))
                             }
-                            print(StudentInformation.studentInformationArray)
                             completion(result: StudentInformation.studentInformationArray)
                         }
                     }
@@ -105,7 +104,7 @@ class NetworkClient: NSObject {
     
     func postLocation(method: String, methodType: String, location:String,link:String,lat:String,long:String) {
         
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/\(method)")!)
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://parse.udacity.com/parse/classes/\(method)")!)
         request.HTTPMethod = methodType
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
@@ -149,7 +148,7 @@ class NetworkClient: NSObject {
         
         let parameters:[String:AnyObject] = [ "where" : NSString(data: data!, encoding: NSUTF8StringEncoding)! ]
         
-        let urlQueryString = "https://api.parse.com/1/classes/StudentLocation?" + encodeParameters(parameters)
+        let urlQueryString = "https://parse.udacity.com/parse/classes/StudentLocation?" + encodeParameters(parameters)
         
         print(urlQueryString)
         
